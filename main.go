54763de5f72bee61e21ce2701f11a84ebb2fa7e5
Kenric D'Souza <AzureByte@users.noreply.github.com>
2018-03-10 14:05:48 +0530

    Added a file walk test to build the poem database in memory when the program loads.

--- main.go
+++ main.go
@@ -3,15 +3,21 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
 	"strings"
 )
 
 var OpenWeatherApiKey = ""
 
 func main() {
+	fmt.Println(getTitleAuthor("..\\public-domain-poetry\\poems\\W-M-MacKeracher-Milton.txt"))
+	poemlist := populatePoemList()
+	fmt.Println(poemlist)
 	http.HandleFunc("/hello", hello)
 	http.HandleFunc("/weather/", weatherHandler)
+	http.HandleFunc("/poems", listPoemsHandler)
 	fmt.Println("Starting server...")
 	fmt.Println("The Server lives! [CTRL+C to exit]")
 	http.ListenAndServe(":8080", nil)
@@ -59,4 +65,67 @@ func weatherHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	json.NewEncoder(w).Encode(data)
+}
+
+type poem struct {
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+func populatePoemList() []poem {
+	files, err := ioutil.ReadDir("../public-domain-poetry")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	p := make([]poem, len(files))
+
+	for i, ele := range files {
+		p[i] = visit("../public-domain-poetry/"+ele.Name(), ele)
+	}
+	return p
+}
+
+func listPoemsHandler(w http.ResponseWriter, r *http.Request) {
+	data := "data"
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	json.NewEncoder(w).Encode(data)
+}
+
+func visit(path string, f os.FileInfo) poem {
+	t, a := getTitleAuthor(path)
+
+	p := poem{Title: t, Author: a}
+
+	return p
+}
+
+//Very specific function for the data we have.
+func getTitleAuthor(path string) (string, string) {
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		fmt.Println(err)
+	}
+	title, author := make([]byte, 0, 0), make([]byte, 0, 0)
+	i := 10
+	for ; i < len(b); i++ {
+		if b[i] == ' ' {
+			continue
+		}
+		author = append(author, b[i])
+		if b[i] == ':' {
+			break
+		}
+	}
+	for ; i < len(b); i++ {
+		if b[i] == ' ' {
+			continue
+		}
+		title = append(title, b[i])
+		if b[i] == '-' {
+			break
+		}
+	}
+
+	return string(title), string(author)
 }
